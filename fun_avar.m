8852994db5b8a3b6da389a61edaecf97f2de4211
gabriellnuness <40604481+gabriellnuness@users.noreply.github.com>
2022-10-29 13:07:21 -0300

    homemade Allan variance algorithm

---
+++ fun_avar.m
@@ -0,0 +1,26 @@
+function [avar, taus] = fun_avar(data, m, fs)
+% [avar, taus] = fun_avar(data, m, fs) returns the Allan
+% variance and the correlation time array taus.
+%
+% when compared to the MATLAB allanvar function there is a difference in
+% running time, this is 6.5 longer and the error is negligible 
+% 
+% Author: stnk
+    
+    % Correlation time array
+    taus = m/fs;
+
+    % Half vectorized approach of equation from
+    % IEEE 952-2020, p. 75
+    N = length(data);
+    last_tau = length(m);
+    data_sum = cumsum(data)/fs;
+    avar = zeros(last_tau, 1);
+    for k = 1:last_tau
+        avar(k) = sum((data_sum(k+2*m(k):N) ...
+                        - 2*data_sum(k+m(k):N-m(k)) ...
+                        + data_sum(k:N-2*m(k))).^2 );
+    end
+    avar = avar./(2.*taus.^2 .* (N-2*m));
+
+end

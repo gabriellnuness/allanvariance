8fa2e16dd0336f8c63ecd2cd6550f993bd996652
gabriellnuness <40604481+gabriellnuness@users.noreply.github.com>
2022-10-31 22:26:18 -0300

    Correlation time array modified - N/2 as last m will not work because it exceeds the number of elements in the data array.

--- fun_tau_array.m
+++ fun_tau_array.m
@@ -1,10 +1,12 @@
 function m = fun_tau_array(N, pts, optm)
 %% Function to generate time array for Allan variance
-% optm = "optimized" --> logspace array
-% optm = "all" --> complete array
-
-%     max_m = 2^floor(log2(N/2));     % Max m in power of 2
-    max_m = (N-1)/2;
+% optm = "optimized"    --> logspace array
+% optm = "all"          --> complete array
+    
+    % Most AVAR algorithms do not allow the last m value to be floor(N/2).
+    % Apparently, the last m must be smaller than N/2 - length(m)
+    max_m = 2^floor(log2(N/2));     % Max m in power of 2
+    
     if optm == "all" 
         m = 2:max_m;
     elseif optm == "optimized" 
@@ -12,5 +14,5 @@ function m = fun_tau_array(N, pts, optm)
     end
     m = floor(m);        
     m = unique(m);
-
+    
 end

63c5b07a51a91dd3839a13bfd811eb42ab936c7d
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2022-11-11 11:46:22 -0300

    Allan deviation working in Julia
    It is slow, but working

--- julia/calc_avar.jl
+++ julia/calc_avar.jl
@@ -3,54 +3,70 @@ using DelimitedFiles
 using Plots
 using AllanDeviations
 
-# include("fun_tau_array.jl")
-# include("fun_avar.jl")
-
+@time begin
 # Importing file
-sensor  = "gyroscope"
-file    = "example_data.txt"
-fs      = 100   # [Hz]
+sensor  = "gyroscope";
+file    = "example_data.txt";
+fs      = 100;   # [Hz]
 println("Chosen file: ",file)
 
-path = pwd()*"\\"
-data = readdlm("$path$file", '\n')
+path = pwd()*"\\";
+data = readdlm("$path$file", '\n');
 
 # Creating correlation time array
-N = length(data)
-t = LinRange(0, (N-1)/fs, N)
+N = length(data);
+t = LinRange(0, (N-1)/fs, N);
+end
 
+# Plot big dataset is not working in julia
 # plot(t/60/60, data,
-    # xlabel = "Time [h]",
-    # ylabel = sensor)
+#     xlabel = "Time [h]",
+#     ylabel = sensor)
 
 # Calculating correlation time array
-pts = 1000
-optm = "optimized" 
-
+pts = 1000;
 max_m = 2^floor(log2(N/2)) 
-if optm == "all" 
-    m = 2:max_m;
-elseif optm == "optimized" 
-    m = 10 .^(range(0, stop=log10(max_m), length=pts));
-end
-m = floor.(m);        
+m = 10 .^(range(0, stop=log10(max_m), length=pts));
+m = floor.(Int, m);     
 m = unique(m);
 
+@time begin
 # Calculating Allan variance
-Ω = data;
-θ = cumsum(Ω[:,1])/fs;
-    
+Ω = data[:,1];
+θ = cumsum(Ω)/fs;
+
 tau0 = 1/fs;
 taus = m*tau0;      # Cluster durations
 
 avar = Float64[];
-# Half vectorized approach
+# Homemade Allan variance calculation
 for k = 1:length(m)
-    avar(k) = sum( (θ(k+2*m(k):N)
-                        - 2*θ(k+m(k):N-m(k))
-                        + θ(k:N-2*m(k))).^2 );
+    avar = [avar; sum( (θ[k+2*m[k]:N]
+                        - 2*θ[k+m[k]:N-m[k]]
+                        + θ[k:N-2*m[k]]).^2) ];
 end
-avar = avar./(2 .* taus.^2 .* (N-2*m));
+avar = avar./(2 .* taus.^2 .* (N.-2*m));
 
+end
 
-plot(taus,avar)
+# Allan deviation from library
+@time begin
+
+result = allandev(Ω, 100.0, frequency = true, 
+    taus = 1.05) # log 1.001 space among taus
+
+scatter(result.tau, result.deviation,
+    xscale = :log10, yscale = :log10)
+end
+
+plot(taus, avar,
+    title="Allan variance",
+    xaxis=:log, yaxis=:log,
+    xlabel="Correlation time [s]",ylabel="Ω [deg/h]",
+    linewidth=2,legend=false)
+
+plot(taus, sqrt.(avar),
+    title="Allan deviation",
+    xaxis=:log, yaxis=:log,
+    xlabel="Correlation time [s]",ylabel="Ω [deg/h]",
+    linewidth=2,legend=false)

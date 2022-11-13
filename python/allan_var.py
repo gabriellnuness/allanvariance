397b2eef7f92e2fa89f413e275fb1cfbe05da7b7
gabriellnuness <40604481+gabriellnuness@users.noreply.github.com>
2022-11-13 12:48:25 -0300

    python code working with standard settings

---
+++ python/allan_var.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+import pandas as pd
+import pylab as plt
+import allantools
+
+# Importing data
+file = "example_data.txt"
+fs = 100 # Hz
+
+# pandas read csv is super fast
+data  = pd.read_csv(file, header=None, names=["Sensor data"])
+# to use allantools the data needs to be numpy array    
+data = data.to_numpy()
+
+# Creating time vector
+N = len(data)
+t = np.linspace(0, (N-1)/fs, N) # time in seconds
+
+fig = plt.plot(t/60/60, data,
+    linewidth=.8)
+plt.xlabel('Time [h]')
+plt.ylabel('Sensor data [units]')
+plt.show()
+
+# Calculating overlapping Allan variance
+(taus, adev, ade, adn) = allantools.oadev(data, rate=fs, data_type="freq")
+
+fig = plt.loglog(taus, adev,'.')
+plt.xlabel("Correlation time [s]")
+plt.ylabel("Allan deviation [units]")
+plt.show()

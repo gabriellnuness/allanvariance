98a55bbfc206b43cc8cfeb57953b529cba31af36
gabriellnuness <40604481+gabriellnuness@users.noreply.github.com>
2022-11-26 15:13:17 -0300

    julia avar conditions check

--- julia/fun_avar.jl
+++ julia/fun_avar.jl
@@ -1,19 +1,23 @@
 function fun_avar(Ω, fs, m)
-   
-θ = cumsum(Ω)/fs;
-N = length(Ω)
 
-tau0 = 1/fs;
-taus = m*tau0;      # Cluster durations
-
-avar = zeros(length(m),1);
-# Homemade Allan variance calculation
-for (k, τ) in enumerate(m)
-    avar[k] = sum( (θ[k+2*τ:N]
-                    - 2*θ[k+τ:N-τ]
-                    + θ[k:N-2*τ]).^2);
-    avar[k] = avar[k]/(2* (τ/fs)^2 * (N-2*τ));
-end
+    if length(Ω)<3
+        error("Allan variance needs at least 3 data points")
+    end
     
-(taus, avar)
+    θ = cumsum(Ω)/fs;
+    N = length(Ω)
+
+    tau0 = 1/fs;
+    taus = m*tau0;      # Cluster durations
+
+    avar = zeros(length(m),1);
+    # Homemade Allan variance calculation
+    for (k, τ) in enumerate(m)
+        avar[k] = sum( (θ[k+2*τ:N]
+                        - 2*θ[k+τ:N-τ]
+                        + θ[k:N-2*τ]).^2);
+        avar[k] = avar[k]/(2* (τ/fs)^2 * (N-2*τ));
+    end
+        
+    (taus, avar)
 end
--- julia/fun_tau_array.jl
+++ julia/fun_tau_array.jl
@@ -1,12 +1,21 @@
 function fun_tau_array(N, pts)
-# Function to generate time array for Allan variance
-    
-# Calculating correlation time array
-pts = 1000;
-max_m = 2^floor(log2(N/2)) 
-m = 10 .^(range(0, stop=log10(max_m), length=pts));
-m = floor.(Int,m);     
-m = unique(m);
+    # Function to generate time array for Allan variance
+    # Input: 
+        # N     → length of input array
+        # pts   → number of correlation points 
+    # Output:
+        # m     → array of Allan variance correlation time
 
-(m)
+    # Check input size
+    if pts > N/2
+        error("Number of points must be less or equal N/2")
+    end
+
+    # Calculating correlation time array
+    max_m = 2^floor(log2(N/2)); 
+    m = 10 .^(range(0, stop=log10(max_m), length=pts));
+    m = floor.(Int,m);     
+    m = unique(m);
+
+    (m)
 end

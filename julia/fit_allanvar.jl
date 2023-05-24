22db23b7630f554c86ffaa066cc76338bb6bfa8a
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2023-05-24 10:18:19 -0300

    julia fit warn about negative nosie values

--- julia/fit_allanvar.jl
+++ julia/fit_allanvar.jl
@@ -1,30 +1,42 @@
-function fit_allanvar(taus::Array{Float64}, avar::Array{Float64})
-# Calculate the sensor noise parameters/coefficients from the 
-# Allan Variance data using Weighted Linear Regression
-#
-# ref: 2017_Jerath-Bridging the gap between sensor noise modeling and sensor characterization (https://doi.org/10.1016/j.measurement.2017.09.012 )
+function fit_allanvar(taus::Array{Float64}, adev::Array{Float64})
+"""
+ Calculate the sensor noise parameters/coefficients from the 
+ Allan Variance data using Weighted Linear Regression
+
+  
+ Fitted Allan deviation noise values
+
+ This values result in complex numbers
+ C = avar_fit
+ unit = measurement unit in SI
+
+ Q     --> unit * s
+ arw   --> unit * s^(1/2)
+ bias  --> unit * s^(0)
+ rrw   --> unit * s^(-1/2) 
+ rr    --> unit * s^(-1)
+
+ ref: 2017_Jerath-Bridging the gap between sensor noise modeling and sensor characterization (https://doi.org/10.1016/j.measurement.2017.09.012 )
+"""
     
     ## Weighted Least-Squares Fitting
-    
+    avar = adev.^2
+
     # The weight is approximately to the order of magnitude
     weight = 1 ./avar
     W = diagm(weight)
-
     T = [taus.^(-2) taus.^(-1) taus.^(0) taus.^(1) taus.^(2)]
 
     avar_fit = (T'*W*T)\T'*W*avar
 
-    ## Fitted Allan deviation noise values
-    # This values result in complex numbers
-    # C = avar_fit
-    # unit = measurement unit in SI
-    #
-    # Q     --> unit * s
-    # arw   --> unit * s^(1/2)
-    # bias  --> unit * s^(0)
-    # rrw   --> unit * s^(-1/2) 
-    # rr    --> unit * s^(-1)
-    #
+    # Check non trustable values from fit
+    avar_fit_original = avar_fit
+    for i in eachindex(avar_fit) 
+        if avar_fit[i] < 0
+            avar_fit[i] = 0 # abs(avar_fit[i])
+            @warn("Noise $i value did not fit correctly")
+        end
+    end
 
     # C = 3*Q^2
     # Q^2 = (q^2)/12, where q = quantum step size
@@ -38,8 +50,8 @@ function fit_allanvar(taus::Array{Float64}, avar::Array{Float64})
     # C = R^2 / 2
     rr   = (2*avar_fit[5])^(0.5)
 
-    adev_fit = sqrt.(avar_fit'*T')
-    adev_fit = adev_fit[1,:]
+    avar_fit_original = sqrt.(avar_fit_original'*T')
+    avar_fit_original = avar_fit_original[1,:]
 
-    (adev_fit, Q, arw, bias, rrw, rr)
+    (avar_fit_original, Q, arw, bias, rrw, rr)
 end
